6a84c3b8b5887dab1d65364170e82443ea43298f
Jarred L. Harris <jarredthewitch@gmail.com>
2021-01-10 14:19:47 -0500

    Separate SqlServer implementation of IUserRepository into its own library. This fixes #2.

---
+++ SeithmanSoftware.Login.Database.SqlServer/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeithmanSoftware.Login.Database.SqlServer
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static void AddUserDatabaseSqlDependency(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
--- SeithmanSoftware.Login.Database.SqlServer/UserRepository.cs
+++ SeithmanSoftware.Login.Database.SqlServer/UserRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
 
-namespace SeithmanSoftware.Login.Database
+namespace SeithmanSoftware.Login.Database.SqlServer
 {
     using Models;
 
